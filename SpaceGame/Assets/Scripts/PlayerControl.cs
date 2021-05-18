c47fb2bfe1338ad948fca23c446a3d46f7f225e0
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2021-05-18 16:43:21 +0100

    removed jump

--- SpaceGame/Assets/Scripts/PlayerControl.cs
+++ SpaceGame/Assets/Scripts/PlayerControl.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField]
-    private float movementSpeed, rotationSpeed, jumpForce;
+    //private float movementSpeed, rotationSpeed, jumpForce;
+    private float movementSpeed, rotationSpeed;
 
     Rigidbody rb;
 
     Camera myCamera;
 
-    bool isJumping;
+    //bool isJumping;
 
     public float currentHealth, maximumHealth;
 
@@ -32,11 +33,12 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
-
+        */
 
     }
 
@@ -50,14 +52,14 @@ public class PlayerControl : MonoBehaviour
         Vector3 inputVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         inputVelocity = inputVelocity * movementSpeed * Time.deltaTime;
 
-        if (!isJumping)
-        {
+        //if (!isJumping)
+        //{
             rb.MovePosition(transform.position + (transform.forward * inputVelocity.z) + (transform.right * inputVelocity.x));
-        }
-        else
-        {
-            rb.MovePosition(transform.position + rb.velocity);
-        }
+        //}
+        //else
+        //{
+        //    rb.MovePosition(transform.position + rb.velocity);
+        //}
 
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
@@ -68,14 +70,14 @@ public class PlayerControl : MonoBehaviour
         transform.Rotate(Vector3.up, horizontalRotation);
         myCamera.transform.rotation = myCamera.transform.rotation * Quaternion.Euler(new Vector3(-verticalRotation, 0, 0));
 
-        //find ouyt how to roll a base rotation
+        //find ouyt how to implement a base rotation
         //rb.rotation = Quaternion.Euler(new Vector3(0, Time.deltaTime, 0));
     }
 
-    void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
-    }
+    //void Jump()
+    //{
+    //    rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+    //}
 
     /*// Start is called before the first frame update
     void Start()
